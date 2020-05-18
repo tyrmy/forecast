e38c9a6f69ed6f12d9a69e05c97622a7cdb94f04
Lassi Lehtinen <lokleh@utu.fi>
2020-05-18 22:23:48 +0300

    forecast plotting for cities as a method

--- forecast.py
+++ forecast.py
@@ -1,33 +1,51 @@
 import requests
+
 from xml.etree import ElementTree
+
 from datetime import datetime
 import dateutil.parser as parser
+
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
-url = 'http://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi::forecast::hirlam::surface::point::timevaluepair&place=turku&parameters=temperature&'
+mins = mdates.MinuteLocator(interval=30)
+hours = mdates.HourLocator(2)
+fmt = mdates.DateFormatter('%m-%d %H:%M')
 
-r = requests.get(url)
-print(r.status_code)
-print("----------")
+def plot_forecast(city):
+    url = 'http://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi::forecast::hirlam::surface::point::timevaluepair&place={}&parameters=temperature&'.format(city)
 
-tree = ElementTree.fromstring(r.content)
+    r = requests.get(url)
+    print(r.status_code)
+    print("----------")
 
-dates = []
-values = []
+    tree = ElementTree.fromstring(r.content)
 
-for child in tree[0][0][-1][0]:
-    #print('{}:\t{}'.format(child[0][0].text, child[0][1].text))
-    iso = child[0][0].text
-    value = float(child[0][1].text)
-    datetime = parser.parse(iso)
+    dates = []
+    values = []
 
-    values.append(value)
-    dates.append(datetime)
+    for child in tree[0][0][-1][0]:
+        iso = child[0][0].text
+        value = float(child[0][1].text)
+        datetime = parser.parse(iso)
 
-#print(values, dates)
-fig, ax = plt.subplots()
-ax.set_title('Turun ennuste, Ilmatieteen laitos')
-plt.plot(dates, values)
-fig.autofmt_xdate()
-plt.grid(True)
-plt.show()
+        values.append(value)
+        dates.append(datetime)
+
+    fig, ax = plt.subplots()
+
+    ax.set_title('{}, Ilmatieteen laitos'.format(city))
+    ax.set_ylabel('Temperature')
+    ax.set_xlabel('Time')
+
+    #ax.xaxis.set_major_locator(hours)
+    ax.xaxis.set_major_formatter(fmt)
+    ax.xaxis.set_minor_locator(mins)
+    plt.plot(dates, values)
+    fig.autofmt_xdate()
+    plt.grid(True)
+
+    plt.show()
+
+if __name__ == "__main__":
+    plot_forecast('turku')
