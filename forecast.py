ce39978307df3862da42cee7b5eef7f935a0d94f
Lassi Lehtinen <lokleh@utu.fi>
2020-05-18 20:52:54 +0300

    data plotting

--- forecast.py
+++ forecast.py
@@ -2,6 +2,7 @@ import requests
 from xml.etree import ElementTree
 from datetime import datetime
 import dateutil.parser as parser
+import matplotlib.pyplot as plt
 
 url = 'http://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi::forecast::hirlam::surface::point::timevaluepair&place=turku&parameters=temperature&'
 
@@ -17,10 +18,16 @@ values = []
 for child in tree[0][0][-1][0]:
     #print('{}:\t{}'.format(child[0][0].text, child[0][1].text))
     iso = child[0][0].text
-    value = child[0][1].text
+    value = float(child[0][1].text)
     datetime = parser.parse(iso)
 
     values.append(value)
     dates.append(datetime)
 
 #print(values, dates)
+fig, ax = plt.subplots()
+ax.set_title('Turun ennuste, Ilmatieteen laitos')
+plt.plot(dates, values)
+fig.autofmt_xdate()
+plt.grid(True)
+plt.show()
