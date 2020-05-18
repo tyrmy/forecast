5d6d31c16dc40cdbc24c5ede6958d4aff56c74c6
Lassi Lehtinen <lokleh@utu.fi>
2020-05-18 20:16:34 +0300

    datetime conversion from iso

--- forecast.py
+++ forecast.py
@@ -1,5 +1,7 @@
 import requests
 from xml.etree import ElementTree
+from datetime import datetime
+import dateutil.parser as parser
 
 url = 'http://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi::forecast::hirlam::surface::point::timevaluepair&place=turku&parameters=temperature&'
 
@@ -8,9 +10,17 @@ print(r.status_code)
 print("----------")
 
 tree = ElementTree.fromstring(r.content)
-#print(tree)
-#print(tree.tag)
+
+dates = []
+values = []
 
 for child in tree[0][0][-1][0]:
-    #print(child.tag)
-    print('{}:\t{}'.format(child[0][0].text, child[0][1].text))
+    #print('{}:\t{}'.format(child[0][0].text, child[0][1].text))
+    iso = child[0][0].text
+    value = child[0][1].text
+    datetime = parser.parse(iso)
+
+    values.append(value)
+    dates.append(datetime)
+
+#print(values, dates)
