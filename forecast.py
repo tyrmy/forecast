b34d492333220c79849e466dd7d1d7d6c254745c
Lassi Lehtinen <lokleh@utu.fi>
2020-05-18 20:10:07 +0300

    printing data

--- forecast.py
+++ forecast.py
@@ -5,6 +5,12 @@ url = 'http://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&s
 
 r = requests.get(url)
 print(r.status_code)
+print("----------")
 
 tree = ElementTree.fromstring(r.content)
 #print(tree)
+#print(tree.tag)
+
+for child in tree[0][0][-1][0]:
+    #print(child.tag)
+    print('{}:\t{}'.format(child[0][0].text, child[0][1].text))
