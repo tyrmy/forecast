cdbe246d929ecd7e8c5a5d5861deae7138ffda86
Lassi Lehtinen <lokleh@utu.fi>
2020-05-22 16:25:58 +0300

    grid and overlay plot, changed fmi

--- OWSforecast.py
+++ OWSforecast.py
@@ -9,12 +9,10 @@ from owslib.wfs import WebFeatureService
 if __name__ == "__main__":
     wfs = WebFeatureService(url='https://opendata.fmi.fi/wfs?request=GetCapabilities', version='2.0.0')
     print(wfs.identification.title)
-    #wfs.get_schema('BsWfs:BsWfsElement')
-    #for idx, storedquery in enumerate(wfs.storedqueries):
-    #    print(idx, ': ', storedquery.id, ' -- ', storedquery.title)
-    #print(list(wfs.contents))
+    for idx, storedquery in enumerate(wfs.storedqueries):
+        print(idx, ': ', storedquery.id, ' -- ', storedquery.title)
 
-    storedquery = wfs.storedqueries[26]
+    storedquery = wfs.storedqueries[104]
     print('\nStoredQuery parameters for %s' % storedquery.id)
     for parameter in storedquery.parameters:
         print(parameter.name, parameter.type)
@@ -22,15 +20,11 @@ if __name__ == "__main__":
     #exit()
     print('\nDownload data from %s' % wfs.identification.title)
     response = wfs.getfeature(
-            storedQueryID='fmi::forecast::hirlam::surface::cities::timevaluepair',
+            storedQueryID='fmi::observations::weather::multipointcoverage',
             #storedQueryID='fmi::forecast::hirlam::surface::cities::multipointcoverage',
             #storedQueryID='fmi::forecast::hirlam::surface::cities::simple',
-            storedQueryParams={'timestep':'60', 
-                                'parameters':'temperature', 
-                                'maxlocations':'1', 
-                                'starttime':'2020-05-21T00:00:00Z',
-                                'endtime':'2020-05-22T00:00:00Z',
-                                'geoid':'633679'
+            storedQueryParams={'place':'Turku', 
+                                'parameters':'temperature,humidity,pressure,totalcloudcover', 
                                 })
     s = response.read().decode()
     print(s)
--- forecast.py
+++ forecast.py
@@ -17,10 +17,10 @@ import matplotlib.dates as mdates
 from scipy.signal import savgol_filter
 import pandas as pd
 
-def plot_forecasts(citys):
+def grid_forecasts(citys):
     """ Fetch data from ilmatieteen laitos and plot results for a city specified """
     # Create figure
-    fig, ax = plt.subplots(2,2, figsize=(6,6))
+    fig, ax = plt.subplots(2,2, figsize=(7,7))
     axes = ax.flatten()
     param = 'temperature'
 
@@ -28,7 +28,10 @@ def plot_forecasts(citys):
         url = 'http://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi::forecast::harmonie::hybrid::point::simple&place={city}&parameters={param}&timestep=30'.format(city=city, param=param)
         r = requests.get(url)
         print(city)
-        print(r.status_code)
+        print('Success %r' % r)
+        print('Elapsed time: ', r.elapsed)
+        print('Status code:, ', r.status_code)
+        print('Code status: ', r.reason)
         print("----------")
 
         dates = []
@@ -49,7 +52,7 @@ def plot_forecasts(citys):
         df['temperature'] = values
         df['datetime'] = dates
         df = df.set_index('datetime')
-        axes[idx].set_ylim(bottom=0, top=20)
+        axes[idx].set_ylim(bottom=5, top=20)
         axes[idx].grid(True, color='r')
         axes[idx].set_title(city)
 
@@ -76,10 +79,73 @@ def plot_forecasts(citys):
                 s=50,
                 ax=axes[idx])
 
-    #ax.legend(citys)
+    fig.suptitle("Ilmatieteenlaitos, ennusteet")
+    plt.show()
+
+
+def plot_forecasts(citys):
+    """ Fetch data from ilmatieteen laitos and plot results for a city specified """
+    # Create figure
+    fig, ax = plt.subplots(figsize=(7,7))
+    param = 'temperature'
+
+    for idx, city in enumerate(citys):
+        url = 'http://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi::forecast::harmonie::hybrid::point::simple&place={city}&parameters={param}&timestep=30'.format(city=city, param=param)
+        r = requests.get(url)
+        print(city)
+        print('Success %r' % r)
+        print('Elapsed time: ', r.elapsed)
+        print('Status code:, ', r.status_code)
+        print('Code status: ', r.reason)
+        print("----------")
+
+        dates = []
+        values = []
+
+        tree = ElementTree.fromstring(r.content)
+        # Parse dates and values form xml
+        for child in tree:
+            iso = child[0][1].text
+            temp = float(child[0][3].text)
+            datetime = parser.parse(iso) + timedelta(hours=3)
+            values.append(temp)
+            dates.append(datetime)
+
+        # Create dataframe
+        df = pd.DataFrame()
+        df['temperature'] = values
+        df['datetime'] = dates
+        df = df.set_index('datetime')
+        ax.set_ylim(bottom=5, top=20)
+
+        # Group by dates and find local maximum
+        tmax = df.loc[df.groupby(pd.Grouper(freq='D')).idxmax().iloc[:,0]]
+        tmax = tmax.reset_index()
+
+        # Group by dates and find local minimum
+        tmin = df.loc[df.groupby(pd.Grouper(freq='D')).idxmin().iloc[:,0]]
+        tmin = tmin.reset_index()
+
+        # Plot line, max and min scatter
+        df.plot(y='temperature',
+                grid=True,
+                ax=ax)
+        tmax.plot.scatter(y='temperature',
+                x='datetime',
+                c='red',
+                s=50,
+                ax=ax)
+        tmin.plot.scatter(y='temperature',
+                x='datetime',
+                c='blue',
+                s=50,
+                ax=ax)
+
+    ax.grid(True)
+    plt.legend(citys)
     fig.suptitle("Ilmatieteenlaitos, ennusteet")
     plt.show()
 
 if __name__ == "__main__":
-    citys = ['kuopio', 'rauma', 'turku', 'vantaa']
+    citys = ['Kuopio', 'Rauma', 'Turku', 'Vantaa']
     plot_forecasts(citys)
